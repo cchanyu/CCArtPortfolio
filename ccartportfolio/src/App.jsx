23da0228669d91009ffd525aff22d3dc5a5ca6ac
cchanyu <chanyu96@gmail.com>
2023-03-19 12:09:44 -0400

    CC - initial restart

---
+++ ccartportfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+const App = () => {
+    return (
+        <div>
+            <Spline scene="https://prod.spline.design/iSTpEEfVMef0QNyF/scene.splinecode" />
+        </div>
+    )
+}
+
+export default App;
